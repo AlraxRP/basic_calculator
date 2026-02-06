bd6ae80c74af4bb7b31fa05fb9866beae9fffc9d
Alexis <alexisrp_4@hotmail.com>
2026-02-05 19:41:50 -0600

    Add operate function

--- js/script.js
+++ js/script.js
@@ -17,5 +17,34 @@ function divide(num1, num2)
 {
     if(num2 === 0) return null;
 
-    return num1 / num2;
+    return parseFloat((num1 / num2).toFixed(2));
 }
+
+function operate(num1, num2, operator)
+{
+    switch(operator)
+    {
+        case "+":
+            console.log(add(num1, num2));
+            break;
+
+        case "-":
+            console.log(subtract(num1, num2));
+            break;
+
+        case "*":
+            console.log(multiply(num1, num2));
+            break;
+
+        case "/":
+            console.log(divide(num1, num2));
+            break;
+
+        default:
+            console.log("Error");
+    }
+}
+
+let num1;
+let num2;
+let operator;
